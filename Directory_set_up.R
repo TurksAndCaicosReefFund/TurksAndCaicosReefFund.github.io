0e9f3037a766f19c547c6b2058584aafadc5a2df
TCReef <michelle@tcreef.org>
2026-04-23 15:21:35 +0100

    set up

---
+++ Directory_set_up.R
@@ -0,0 +1,9 @@
+#set working directory
+setwd("C:/Users/miche/Dropbox/TCRF/GitHub/TCReef/Website")
+
+# create folder structure
+dir.create("code/rcode", recursive = TRUE, showWarnings = FALSE)
+dir.create("code/unix", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/confidential", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/too_large_files", recursive = TRUE, showWarnings = FALSE)
+dir.create("outputs/figures", recursive = TRUE, showWarnings = FALSE)
---
+++ End_of_day.R
@@ -0,0 +1,8 @@
+#End of day#
+
+#in terminal below, run the following code
+
+#git status
+#git add code/
+  #git commit -m "brief description of what changed"
+#git push origin main
